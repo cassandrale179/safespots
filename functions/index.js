35d59c4e956069d72ac187ef03021018050fb6d0
Cassandra Le <lenguyetminh1998@gmail.com>
2018-04-21 14:04:30 -0400

    DONE: firebase config

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
+
+exports.sendMessageNotificaton = functions.database
+    .ref('/buildings/{buildingID}').onWrite(event => {
+
+    }); 
