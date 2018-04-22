b1234f586bb2b5cc8a86c3f995501efa5246b9c7
Cassandra Le <lenguyetminh1998@gmail.com>
2018-04-21 23:41:36 -0400

    FInish building student list

--- src/pages/emergency/emergency.ts
+++ src/pages/emergency/emergency.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the EmergencyPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,11 +9,46 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EmergencyPage {
 
+    //---- VARIABLES -----
+    studentsArr: any;
+    rescueArr: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+
+//----------- LOAD STUDENTS -------
   ionViewDidLoad() {
-    console.log('ionViewDidLoad EmergencyPage');
+      this.studentsArr = [];
+      var names = ["Joseph", "Ann", "Vivian", "Emily", "Cheng"];
+      var status = ["Unknown", "No Reported Injury", "Injured", "Injured", "Unknown"];
+      var miles = ["0.2", "0.5", "0.7", "0.9", "1"];
+      for (var i = 0; i < names.length; i++){
+          let student = {
+              name: names[i],
+              status: status[i],
+              miles: miles[i],
+              pic: "../assets/imgs/" + names[i] + ".jpg"
+          }
+          this.studentsArr.push(student);
+      }
+
+
+
+      this.rescueArr = [];
+      var names2 = ["Britney", "Kean", "Hong"];
+      var status2  = ["Safe", "Safe", "Safe"];
+      var miles2 = ["3", "4", "5"];
+      for (var i = 0; i < names2.length; i++){
+          let s = {
+              name: names2[i],
+              status: status2[i],
+              miles: miles2[i],
+              pic: "../assets/imgs/" + names2[i] + ".jpg"
+          }
+          this.rescueArr.push(s);
+      }
+
   }
 
 }
