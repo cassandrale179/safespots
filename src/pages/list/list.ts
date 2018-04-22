a7e7706e472d32829a2c0d5cc1adf4bb99c33f23
Cassandra Le <lenguyetminh1998@gmail.com>
2018-04-22 08:41:53 -0400

    DONE: different marker color

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,9 +1,9 @@
 import { Component, ViewChild} from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {} from '@types/googlemaps';
-import { NativeStorage } from '@ionic-native/native-storage';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Storage } from '@ionic/storage';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-list',
@@ -21,13 +21,29 @@ export class ListPage {
   hydrantsArr: any;
   markerArr: any;
 
-  constructor(public navCtrl: NavController, private afData: AngularFireDatabase, public navParams: NavParams, private storage: Storage) {
+  constructor(
+      public navCtrl: NavController,
+      private afData: AngularFireDatabase,
+      public navParams: NavParams,
+      private storage: Storage,
+      private toastCtrl: ToastController) {
 
   }
 
 
   //---------------- INIT MAP -------------
       async ngOnInit() {
+      let toast = this.toastCtrl.create({
+        message: 'Red marker are gun shots, blue are safe buildings',
+        duration: 3000,
+        position: 'top'
+      });
+      toast.onDidDismiss(() => {
+        console.log('Dismissed toast');
+      });
+      toast.present();
+
+
 
         var mapProp = {
              center: new google.maps.LatLng(40.4995488, -74.4443186),
@@ -43,10 +59,18 @@ export class ListPage {
                 let lng = markerInfo['lng']
                 let marker = new google.maps.Marker({
                     position: new google.maps.LatLng(lat, lng),
-                    map: this.fireMap
+                    map: this.fireMap,
+                    icon: '../assets/imgs/blue.png'
                 })
 
             })
+        })
+
+
+        //------- GUN SHOT MARKER GOES HERE ------
+        let gunshot = new google.maps.Marker({
+            position: new google.maps.LatLng(40.4995488, -74.4443186),
+            map: this.fireMap,
         })
     }
 }
